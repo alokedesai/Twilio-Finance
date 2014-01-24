ecf787ed70e3ae7a3b036f524b7d4e8f9ec3313c
Aloke Desai <aloke.desai@gmail.com>
2014-01-24 10:41:15 -0800

    support for more Yahoo API methods

--- app.py
+++ app.py
@@ -15,38 +15,32 @@ ordrin_api = ordrin.APIs("aDn9SOwqDwmdmAmi8Qcb-LGre8M5sY_-q15dXet9zVw")
 
 
 client = TwilioRestClient(account_sid, auth_token)
-def createText(result):
+def createText(method, companies):
 	output = ""
-	for item in result:
-		output += item["na"] + "-" + item["addr"]
-		# new line for text
-		output += "\n"
+	for company in companies:
+		output += company.strip() + ": " + method(company.strip()) + "\n"
 	return output
 @app.route("/")
 def index():
 	# return str(ystockquote.get_last_trade_price("AAPL"))
-	return "Index"
+	return createText(ystockquote.get_last_trade_price, ["TWTR"])
 
 
 @app.route("/sms", methods=["POST"])
 def sms():
 	# query HackFood to get possible delivery
 	text = str(request.form["Body"].lower())
-
+	companies = text.split(",")
 	if (text.startswith("change ")):
-		text = text.replace("change" , "")
-
-		output = ""
-		companies = text.split(",")
-		for company in companies:
-			output += company.strip().upper() + ": " + ystockquote.get_change_percent_change(company.strip()) + "\n"
+		output = createText(ystockquote.get_change_percent_change, companies)
+		resp = twilio.twiml.Response()
+		resp.message(output)
+	elif (text.startswith("pe ")):
+		output = createText(ystockquote.get_pe, companies)
 		resp = twilio.twiml.Response()
 		resp.message(output)
 	else:
-		companies = text.split(",")
-		output = ""
-		for company in companies:
-			output += company.strip().upper() + ": " + ystockquote.get_last_trade_price(company.strip()) + "\n"
+		output = createText(ystockquote.get_last_trade_price, companies)
 		resp = twilio.twiml.Response()
 		resp.message(output)
 	# message = client.messages.create(to="+15135605548", from_="+15132838068", body= text_body)
