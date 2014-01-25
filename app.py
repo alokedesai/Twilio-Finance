6560c334f1edb0c7a7266238a03effe07546d1c8
Aloke Desai <aloke.desai@gmail.com>
2014-01-24 22:22:59 -0800

    added support for price method

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ from twilioInfo import *
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.do')
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
-ordrin_api = ordrin.APIs("aDn9SOwqDwmdmAmi8Qcb-LGre8M5sY_-q15dXet9zVw")
-
-
-
 
 
 client = TwilioRestClient(account_sid, auth_token)
 def createText(method, companies):
 	output = ""
 	for company in companies:
+		# properly format the output
 		output += company.strip().upper() + ": " + method(company.strip()) + "\n"
 	return output
 @app.route("/")
@@ -31,7 +28,13 @@ def sms():
 	# query HackFood to get possible delivery
 	text = str(request.form["Body"].lower())
 
-	if (text.startswith("change ")):
+	if (text.startswith("price ")):
+		text = text.replace("price", "")
+		output = createText(ystockquote.get_last_trade_price, text.split(","))
+		resp = twilio.twiml.Response()
+		resp.message(output)
+		return str(resp)
+	elif (text.startswith("change ")):
 		text = text.replace("change", "")
 		output = createText(ystockquote.get_change_percent_change, text.split(","))
 		resp = twilio.twiml.Response()
@@ -77,25 +80,6 @@ def sms():
 		resp = twilio.twiml.Response()
 		resp.message(output)
 		return str(resp)
-	
-	# if (text.startswith("change ")):
-	# 	text = text.replace("change","")
-	# 	companies = text.split(",")
-	# 	output = createText(ystockquote.get_change_percent_change, companies)
-	# 	resp = twilio.twiml.Response()
-	# 	resp.message(output)
-	# elif (text.startswith("pe ")):
-	# 	text = text.replace("pe","")
-	# 	companies = text.split(",")
-	# 	output = createText(ystockquote.get_pe, companies)
-	# 	resp = twilio.twiml.Response()
-	# 	resp.message(output)
-	# else:
-	# 	output = createText(ystockquote.get_last_trade_price, companies)
-	# 	resp = twilio.twiml.Response()
-	# 	resp.message(output)
-	# # message = client.messages.create(to="+15135605548", from_="+15132838068", body= text_body)
-	# return str(resp)
  
 
 if __name__ == '__main__':
